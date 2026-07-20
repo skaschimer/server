deic-dk/nextcloud_server

feat(lib/Sharing): Init
Signed-off-by: provokateurin <kate@provokateurin.de>

lib/private/Server.php
@@ -1320,6 +1320,9 @@ class Server extends ServerContainer implements IServerContainer {
 		}, false);
 		$this->registerAlias(ISnowflakeDecoder::class, SnowflakeDecoder::class);
 
+		$this->registerAlias(\OCP\Sharing\IRegistry::class, \OC\Sharing\Registry::class);
+		$this->registerAlias(\OCP\Sharing\IManager::class, \OC\Sharing\Manager::class);
+
 		$this->connectDispatcher();
 	}
 

skaschimer/server

lib/private/Server.php
@@ -1151,6 +1151,9 @@ class Server extends ServerContainer implements IServerContainer {
 		$this->registerAlias(IJobRuns::class, JobRuns::class);
 		$this->registerAlias(IServerInfo::class, ServerInfo::class);
 
+		$this->registerAlias(\OCP\Sharing\ISharingRegistry::class, \OC\Sharing\SharingRegistry::class);
+		$this->registerAlias(\OCP\Sharing\ISharingManager::class, \OC\Sharing\SharingManager::class);
+
 		$this->connectDispatcher();
 	}
 